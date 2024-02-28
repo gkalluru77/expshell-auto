4e788bcb999ccfd738cf0036b873a0f42c1c88fd
girin <girinath.kalluru@gmail.com>
2024-02-28 12:52:00 +0530

    sample file

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,27 @@
-dnf module disable mysql -y
+log_file=/tmp/out
+color="\e[32m"
 
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "${color} disable mysql default version \e[0m"
+dnf module disable mysql -y  &>> log_file
+echo $?
 
-dnf install mysql-community-server -y
+echo -e "${color} Copying repo file \e[0m"
+cp mysql.repo /etc/yum.repos.d/mysql.repo  &>> log_file
+ echo $?
 
-systemctl enable mysqld
-systemctl start mysqld
+echo -e "${color} Installing mysql \e[0m"
+dnf install mysql-community-server -y  &>> log_file
+ echo $?
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo -e "${color} Enabling mysql default version \e[0m"
+systemctl enable mysqld  &>> log_file
+echo $?
+
+echo -e "${color} Starting mysql  \e[0m"
+systemctl start mysqld  &>> log_file
+ echo $?
+
+echo -e "${color} Changing the default root password \e[0m"
+mysql_secure_installation --set-root-pass ExpenseApp@1  &>> log_file
+echo $?
 
