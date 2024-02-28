f27b5e393ec6638e793a31dcbe4df76d40bbb3a5
girin <girinath.kalluru@gmail.com>
2024-02-28 11:31:24 +0530

    sample file

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,19 @@
+echo -e "\e[31m Installing Nginx \e[0m"
 dnf install nginx -y
 
+echo -e "\e[31m copying reverse proxy  Nginx \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
 
+echo -e "\e[31m Removing default content Nginx \e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[31m extracting fronend zip \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e[31m unzip frontend file \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-
+echo -e "\e[31m Restarting Nginx \e[0m"
 systemctl enable nginx
 systemctl restart nginx
