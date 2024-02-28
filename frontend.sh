11ac1b8add87a5ac47a369eb32aa9291e820d7ac
girin <girinath.kalluru@gmail.com>
2024-02-28 11:55:31 +0530

    sample file

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,22 @@
-echo -e "\e[31m Installing Nginx \e[0m"
-dnf install nginx -y &>> /tmp/out
+log_file=/tmp/out
+color="\e[31m"
 
-echo -e "\e[31m copying reverse proxy  Nginx \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>> /tmp/out
+echo -e "${color} Installing Nginx \e[0m"
+dnf install nginx -y &>> log_file
 
-echo -e "\e[31m Removing default content Nginx \e[0m"
-rm -rf /usr/share/nginx/html/* &>> /tmp/out
+echo -e "${color} copying reverse proxy  Nginx \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>> $log_file
 
-echo -e "\e[31m extracting fronend zip \e[0m" &>> /tmp/out
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>> /tmp/out
+echo -e "${color} Removing default content Nginx \e[0m"
+rm -rf /usr/share/nginx/html/* &>> $log_file
 
-echo -e "\e[31m unzip frontend file \e[0m"
+echo -e "${color} extracting frontend zip \e[0m" &>> $log_file
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>> $log_file
+
+echo -e "${color} unzip frontend file \e[0m"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>> /tmp/out
+unzip /tmp/frontend.zip &>> $log_file
 
-echo -e "\e[31m Restarting Nginx \e[0m"
+echo -e "${color} Restarting Nginx \e[0m"
 systemctl enable nginx
 systemctl restart nginx
