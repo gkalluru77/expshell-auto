d9ff7bad992e7f6297ac905d7a6a8a39f7ee7764
girin <girinath.kalluru@gmail.com>
2024-02-28 12:14:42 +0530

    sample file

--- frontend.sh
+++ frontend.sh
@@ -8,13 +8,14 @@ echo $?
 echo -e "${color} copying reverse proxy  Nginx \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>> $log_file
 echo $?
+
 echo -e "${color} Removing default content Nginx \e[0m"
 rm -rf /usr/share/nginx/html/* &>> $log_file
 echo $?
 
 echo -e "${color} extracting frontend zip \e[0m" &>> $log_file
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>> $log_file
-echo $?
+#echo $?
 
 echo -e "${color} unzip frontend file \e[0m"
 cd /usr/share/nginx/html
