46cba1df670b391bad7994bb60ba3e223abdba79
girin <girinath.kalluru@gmail.com>
2024-02-28 12:41:10 +0530

    sample file

--- backend.sh
+++ backend.sh
@@ -2,42 +2,59 @@ log_file=/tmp/out
 color="\e[32m"
 
 echo -e "${color} disable node js default version \e[0m"
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>> log_file
+echo $?
+
 echo -e "${color} enable node js 18 version \e[0m"
-dnf module enable nodejs:18 -y
+dnf module enable nodejs:18 -y &>> log_file
+echo $?
 
 echo -e "${color} install nodeJs \e[0m"
-dnf install nodejs -y
+dnf install nodejs -y &>> log_file
+ echo $?
 
 echo -e "${color} copying backend service file \e[0m"
-cp backend.service /etc/systemd/system/backend.service
+cp backend.service /etc/systemd/system/backend.service &>> log_file
+ echo $?
 
 echo -e "${color} adding application user \e[0m"
-useradd expense
+useradd expense &>> log_file
+ echo $?
 
 echo -e "${color} creating application directory \e[0m"
-mkdir /app
+mkdir /app &>> log_file
+ echo $?
 
 echo -e "${color} downloading the backend application \e[0m"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>> log_file
+   echo $?
 
 echo -e "${color} changing to application directory \e[0m"
-cd /app
+cd /app &>> log_file
+echo $?
+
 echo -e "${color} unzip the application \e[0m"
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip &>> log_file
+ echo $?
 
 echo -e "${color} installing nodejs dependencies \e[0m"
-npm install
+npm install &>> log_file
+  echo $?
 
 echo -e "${color} installing the mysql client to load schema \e[0m"
-dnf install mysql -y
+dnf install mysql -y &>> log_file
+echo $?
 
 echo -e "${color} load schema  \e[0m"
-mysql -h mysql-dev.gdevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql-dev.gdevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>> log_file
+ echo $?
 
 
 echo -e "${color} Restarting the backed service \e[0m"
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
+systemctl daemon-reload &>> log_file
+ echo $?
+systemctl enable backend &>> log_file
+echo $?
+systemctl start backend &>> log_file
+  echo $?
 
